78ac0ebf2873cb23082b1b0c3f0def6a517da051
Alex Via <alexandervia@gmail.com>
2023-01-17 15:31:49 -0600

    genreates dice values upon intialization and renders to each die component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import './App.css';
 import Die from './Die.js'
+import React from "react"
 
 function App() {
+    const [rolledDice, setRolledDice] = React.useState(allNewDice())
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            let rolledNumber = Math.ceil(Math.random() * 6)
+            newDice.unshift(rolledNumber)
+        };
+        return newDice
+      }
+    
+    const diceArray = rolledDice.map(die => <Die value={die} />)
     return (
         <main>
             <container className="dice">
-                <Die value="3" />
-                <Die value="3" />
-                <Die value="3" />
-                <Die value="3" />
-                <Die value="3" />
-                <Die value="3" />
-                <Die value="3" />
-                <Die value="3" />
-                <Die value="3" />
-                <Die value="3" />
+                {diceArray}
             </container>
         </main>
     );
