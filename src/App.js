2622e10008082e3ea7b3d4fab4fe53a2ef0221bc
Alex Via <alexandervia@gmail.com>
2023-01-18 11:44:45 -0600

    holdDie function logs each die's id

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
             let rolledNumber = Math.ceil(Math.random() * 6)
             newDice.push({
                 value: rolledNumber,
-                isHeld: true,
+                isHeld: false,
                 id: i + 1
             })
         };
@@ -22,13 +22,18 @@ function App() {
     function rollDice() {
         setRolledDice(allNewDice)
     }
+
+    function holdDie(id) {
+        console.log(id)
+    }
     
     const diceArray = rolledDice.map(die => 
         <Die 
         key={die.id} 
         value={die.value} 
         isHeld={die.isHeld}
-    />)
+        holdDie={() => holdDie(die.id)}/>
+    )
 
     return (
         <main>
--- src/Die.js
+++ src/Die.js
@@ -3,7 +3,7 @@ export default function Die(props) {
         backgroundColor: props.isHeld ? "#59E391" : "white"
     }
     return (
-        <div className="die" style={styles}>
+        <div className="die" style={styles} onClick={props.holdDie}>
             <h1>{props.value}</h1>
         </div>
     )
