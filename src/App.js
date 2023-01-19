cc947b2af6d5a0a0565a778540c2bf0fb86f7480
Alex Via <alexandervia@gmail.com>
2023-01-19 11:58:18 -0600

    adds title and instructions, useEffect checks for winning conditions

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import React from "react"
 function App() {
     
     const [rolledDice, setRolledDice] = React.useState(allNewDice())
-    
+    const [tenzies, setTenzies] = React.useState(false)
+
     function allNewDice() {
         const newDice = []
         for (let i = 0; i < 10; i++) {
@@ -18,19 +19,27 @@ function App() {
         return newDice
     }
 
+    React.useEffect(() => {
+        const allEqual = arr => arr.every(v => v.value === arr[0].value)
+        const allHeld = arr => arr.every(v => v.isHeld === arr[0].isHeld)
+        allEqual(rolledDice) && allHeld(rolledDice) ?
+            console.log("YOU WIN!!") :
+            console.log("keep rolling")
+    }, [rolledDice])
+
     function rollDice() {
         setRolledDice(prevRolledDice => prevRolledDice.map(die => {
             return die.isHeld ?
-                die :
-                {...die, value: Math.ceil(Math.random() * 6)}
+            die :
+            {...die, value: Math.ceil(Math.random() * 6)}
         }))
     }
-
+    
     function holdDie(id) {
         setRolledDice(prevRolledDice => prevRolledDice.map(die => {
             return die.id === id ? 
-                {...die, isHeld: !die.isHeld} :
-                die
+            {...die, isHeld: !die.isHeld} :
+            die
         }))
     }
     
@@ -40,10 +49,12 @@ function App() {
         value={die.value} 
         isHeld={die.isHeld}
         holdDie={() => holdDie(die.id)}/>
-    )
-
-    return (
-        <main>
+        )
+        
+        return (
+            <main>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice">
                 {diceArray}
             </div>
